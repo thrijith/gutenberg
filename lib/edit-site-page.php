5418ac228ec17e54e18b6fff4d393f1e82a09036
Bernie Reiter <ockham@raz.or.at>
2020-06-04 15:25:29 +0200

    Remove some server-side templateIds logic

--- lib/edit-site-page.php
+++ lib/edit-site-page.php
@@ -144,7 +144,6 @@ function gutenberg_edit_site_init( $hook ) {
 	}
 	$settings['styles'] = gutenberg_get_editor_styles();
 
-	$template_ids      = array();
 	$template_part_ids = array();
 	foreach ( get_template_types() as $template_type ) {
 		// Skip 'embed' for now because it is not a regular template type.
@@ -155,8 +154,7 @@ function gutenberg_edit_site_init( $hook ) {
 
 		$current_template = gutenberg_find_template_post_and_parts( $template_type );
 		if ( isset( $current_template ) ) {
-			$template_ids[ $template_type ] = $current_template['template_post']->ID;
-			$template_part_ids              = $template_part_ids + $current_template['template_part_ids'];
+			$template_part_ids = $template_part_ids + $current_template['template_part_ids'];
 		}
 	}
 
