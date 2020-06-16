95343913ec5d36f7193ef461ae7d53ae9f01b494
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-06-16 16:47:52 +0300

    generalizes the category search handler to all terms

--- packages/edit-navigation/src/index.js
+++ packages/edit-navigation/src/index.js
@@ -35,7 +35,7 @@ import Layout from './components/layout';
  * @return {Promise<Object[]>} List of suggestions
  */
 const fetchLinkSuggestions = async ( search, { perPage = 20 } = {} ) => {
-	const posts = await apiFetch( {
+	const posts = apiFetch( {
 		path: addQueryArgs( '/wp/v2/search', {
 			search,
 			per_page: perPage,
@@ -43,15 +43,15 @@ const fetchLinkSuggestions = async ( search, { perPage = 20 } = {} ) => {
 		} ),
 	} );
 
-	const categories = await apiFetch( {
+	const terms = apiFetch( {
 		path: addQueryArgs( '/wp/v2/search', {
 			search,
 			per_page: perPage,
-			type: 'category',
+			type: 'term',
 		} ),
 	} );
 
-	const formats = await apiFetch( {
+	const formats = apiFetch( {
 		path: addQueryArgs( '/wp/v2/search', {
 			search,
 			per_page: perPage,
@@ -59,7 +59,7 @@ const fetchLinkSuggestions = async ( search, { perPage = 20 } = {} ) => {
 		} ),
 	} );
 
-	return Promise.all( [ posts, categories, formats ] ).then( ( results ) => {
+	return Promise.all( [ posts, terms, formats ] ).then( ( results ) => {
 		return map( flatten( results ), ( post ) => ( {
 			id: post.id,
 			url: post.url,
